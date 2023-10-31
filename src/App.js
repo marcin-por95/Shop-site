382b6c5619ba56103041edc9fee020c58d4be4fd
Marcin Poręba <marcin.poreba1995@gmail.com>
2023-10-31 21:54:58 +0100

    create simple structure website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import HomePage from './components/HomePage';
+import ProductPage from './components/ProductPage';
+import CartPage from './components/CartPage';
+import OrderFormPage from './components/OrderFormPage';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/product/:id" element={<ProductPage />} />
+                <Route path="/cart" element={<CartPage />} />
+                <Route path="/order" element={<OrderFormPage />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
