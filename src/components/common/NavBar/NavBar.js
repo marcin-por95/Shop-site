0dc46bc42dbc3001b195ee580b243a92d5e43180
Marcin Poręba <marcin.poreba1995@gmail.com>
2023-11-02 15:07:32 +0100

    add carousel and img

--- src/components/common/NavBar/NavBar.js
+++ src/components/common/NavBar/NavBar.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './NavBar.scss';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 function Navbar() {
     return (
         <header>
             <h1>L U C H I A N A</h1>
             <nav>
                 <ul>
+                    <a href="/koszyk">
+                        <FontAwesomeIcon icon={faCartShopping} />
+                    </a>
                     <li><a href="#">HOME</a></li>
                     <li><a href="#">SHOP</a></li>
                     <li><a href="#">CONTACT</a></li>
--- src/components/pages/Home/HomePage.js
+++ src/components/pages/Home/HomePage.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import Navbar from '../../../components/common/NavBar/NavBar';
+import Carousel from "nuka-carousel";
 
 function HomePage() {
     return (
         <div>
             <Navbar />
             <main>
+                <Carousel
+                    className="productCarousel"
+                    wrapAround={true}
+                    autoplay={true}
+                    autoplayInterval={3500}
+                >
+                    <img src="/images/image1.jpg" alt="Opis zdjęcia 1" />
+                    <img src="/images/image2.jpg" alt="Opis zdjęcia 2" />
+                    <img src="/images/image3.jpg" alt="Opis zdjęcia 3" />
+                    <img src="/images/image4.jpg" alt="Opis zdjęcia 4" />
+                </Carousel>
+
+
+
                 <section className="collection">
-                    <h2>NEW COLLECTION</h2>
+                    <h2>NASZE PRODUKTY</h2>
                     <h3>JOZY AND MARCO</h3>
                     <p>This collection is all about making a statement with vibrant colors and bold finishes.</p>
                 </section>
